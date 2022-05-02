3967fec8180aa0695990d15ef32a58d22574c133
udFolkesi <oleksiy.salamatov@gmail.com>
2022-05-02 18:40:33 +0300

    2 change

---
+++ Lab 1/Lab 1/Field.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class Field : Element
+    {
+        internal static readonly char[,] Matrix = new char[8, 23];
+        private Mine mine = new Mine();
+
+        // 6, 17
+        public override void Define()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < 8; i++)
+            {
+                for (int k = 0; k < 23; k++)
+                {
+                    if (i > 0 && i < 7 && k > 0 && k < 22)
+                    {
+                        Matrix[i, k] = ' ';
+                    }
+                    else
+                    {
+                        Matrix[i, k] = '#';
+                    }
+
+                    if (i == 0 && k < 13 && k > 9)
+                    {
+                        Matrix[i, k] = ' ';
+                    }
+
+                    if (i == 7 && k < 13 && k > 9)
+                    {
+                        Matrix[i, k] = ' ';
+                    }
+
+                    if (Matrix[i, k] == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(Matrix[i, k]);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                    else
+                    {
+                        Console.Write(Matrix[i, k]);
+                    }
+                }
+
+                Console.WriteLine();
+                this.mine.Traps(i);
+            }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 23; j++)
+                {
+                    if (Matrix[i, j] == 'X')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(Matrix[i, j]);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+
+                    if (Matrix[i, j] == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(Matrix[i, j]);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+
+                    if (Matrix[i, j] == ' ')
+                    {
+                        Console.Write(Matrix[i, j]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab 1/Lab 1/Rules.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class Rules : Element
+    {
+        public override void Define()
+        {
+            // throw new NotImplementedException();
+            Console.SetCursorPosition(20, 1);
+            Console.Write(@"
+                           ============= Rules ================
+                           You can use arrows(◄ ►▲▼) for move
+                           To show hint press.. 
+                           If you want to finish game press Esc
+            ");
+        }
+    }
+}
